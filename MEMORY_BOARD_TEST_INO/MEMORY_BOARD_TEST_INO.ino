b0a42e146ba3564263bb5586ffaaf73c2fbeb0a2
Frank Bösing <f.boesing@gmx.de>
2015-09-06 11:09:49 +0200

    improved DMA, rmv need of DmaSpi

--- MEMORY_BOARD_TEST_INO/MEMORY_BOARD_TEST_INO.ino
+++ MEMORY_BOARD_TEST_INO/MEMORY_BOARD_TEST_INO.ino
@@ -1,4 +1,3 @@
-
 /* Memoryboard code is placed under the MIT license
  * Copyright (c) 2015 Frank Bösing
  *
@@ -22,10 +21,11 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
- 
-#include <SPI.h>
-#include <DmaSpi.h> 
+
+ #include <SPI.h>
+#include <SPIFIFO.h>
 #include <arm_math.h>
+#include <kinetis.h>
 #include "memoryboard.h"
 
 
@@ -34,13 +34,13 @@
 
 DMAMEM uint8_t buf1[BUFSIZE];
 DMAMEM uint8_t buf2[BUFSIZE];
-DMAMEM uint16_t buf3[BUFSIZE3 ];
+DMAMEM uint16_t buf3[BUFSIZE3];
 
 uint32_t laufnr;
 
 
-void setup() { 
- memoryboard_init();          
+void setup() {
+ memoryboard_init();
  while(!Serial);
  Serial.println("Start");
  laufnr = 0;
@@ -48,66 +48,70 @@ void setup() {
 
 
 void checkMem(int num){
-int res;
-uint32_t addr;  
+int m,res;
+uint32_t addr;
 
-  Serial.printf("RDMR %d:0x%x ... ", num, ram_readRDMR(num) );
-  
+  Serial.printf("RAM #%d ... ", num);
+  m = millis();
   res = 0;
+  if (ram_readRDMR(num) != 0x40) {
+	  res = 1;
+  }
   for (addr = 0; addr < 128*1024; addr += BUFSIZE) {
   //for (addr = 0; addr < BUFSIZE; addr += BUFSIZE) {
   if (res == 0) {
     memset(buf1,0x55,BUFSIZE);
-    buf1[0] = 7;
     ram_write(num, buf1, addr, BUFSIZE);
-    ram_read(num, buf2, addr, BUFSIZE);  
+    ram_read(num, buf2, addr, BUFSIZE);
     res = memcmp(buf1,buf2,BUFSIZE);
-  } 
-  if (res==0) {
-    memset(buf1,0xaa,BUFSIZE);     
-    ram_write(num, buf1, addr, BUFSIZE);
-    ram_read(num, buf2, addr, BUFSIZE);    
-    res = memcmp(buf1,buf2,BUFSIZE);  
   }
-  if (res==0) {    
+  if (res==0) {
+    memset(buf1,0xaa,BUFSIZE);
+    ram_write(num, buf1, addr, BUFSIZE);
+    ram_read(num, buf2, addr, BUFSIZE);
+    res = memcmp(buf1,buf2,BUFSIZE);
+  }
+  if (res==0) {
     memset(buf1,0x00,BUFSIZE);
     ram_write(num, buf1, addr, BUFSIZE);
     ram_read(num, buf2, addr, BUFSIZE);
-    res = memcmp(buf1,buf2,BUFSIZE);  
+    res = memcmp(buf1,buf2,BUFSIZE);
   }
 /*
   if (res==0) {
     memset(buf1,0xff,BUFSIZE);
     ram_write(num, buf1, addr, BUFSIZE);
     ram_read(num, buf2, addr, BUFSIZE);
-    res = memcmp(buf1,buf2,BUFSIZE);  
+    res = memcmp(buf1,buf2,BUFSIZE);
   }
-*/   
+*/
   if (res==0) {
     int i;
     for (i=0; i< BUFSIZE3; i++)
       buf3[i] = i;
     ram_write(num, (uint8_t*)&buf3, addr, BUFSIZE);
     ram_read(num, buf2, addr, BUFSIZE3*2);
-    res = memcmp(buf3,buf2,BUFSIZE3);  
-    //Serial.printf("STOP.  0x%x 0x%x", buf3[1], buf2[1]);
+    res = memcmp(buf3,buf2,BUFSIZE3);
   }
   }
-  Serial.printf("%s\r\n",res==0?"OK":"ERROR");
+  m = millis()-m;
+  Serial.printf("%s %ums\r\n",res==0?"OK":"ERROR",m);
   if (res) {
-    Serial.printf("STOP.  0x%x 0x%x", buf1[0], buf2[0]);
+	Serial.printf("Stop.\r\nDump:\r\n");
+	for (int i=0; i<BUFSIZE; i++)
+		Serial.printf("0x%x<->0x%x\r\n", buf1[i], buf2[i]);
     while(1) {;}
   }
-  
+
 }
 
 
 void loop() {
-int num;  
+int num;
   laufnr++;
-  Serial.printf("Lauf %u:\r\n", laufnr);
-  
-  for (num=1; num <=  4; num++) checkMem(num)  ;
+  Serial.printf("Run %u:\r\n", laufnr);
+
+  for (num=1; num <=RAM_NUM_CHIPS; num++) checkMem(num);
   Serial.println();
   delay(300);
 }
--- MEMORY_BOARD_TEST_INO/memoryboard.h
+++ MEMORY_BOARD_TEST_INO/memoryboard.h
@@ -29,301 +29,309 @@
 
 /**************************/
 // Edit here:
-#define SPICONFIG             SPISettings(20000000, MSBFIRST, SPI_MODE0 ) //Speed. 23LC1024 = 20MHz max, see Datasheet.
+
+#define MEMORYBOARD_VERSION 3
+
 #define RAM_NUM_CHIPS         (4) //# of RAMs
 #define RAM_CHIP_SIZE         (128*1024) //Size per RAM
 
 //End "Edit here"
 /**************************/
 
+#include <SPI.h>
+//#include <SPIFIFO.h>
+#include <DMAChannel.h>
+#include <arm_math.h>
+
+#if (MEMORYBOARD_VERSION < 4)
 #define CS0     8
 #define CS1    17
 #define CS2    16
-
-#define SCK   14
-#define MOSI   7
-#define MISO  12
-
-#include <SPI.h>
-#include <arm_math.h>
-#include <DmaSpi.h>
-
-//RAM-Instructions
-#define RAM_INSTR_READ      0x03
-#define RAM_INSTR_WRITE     0x02
-/*
-#define RAM_INSTR_EDIO      0x3B
-#define RAM_INSTR_EQIO      0x38
-#define RAM_INSTR_RSTIO     0xFF
-*/
-#define RAM_INSTR_RDMR      0x05
-#define RAM_INSTR_WRMR      0x01
-
-/*
-#define RAM_BYTEMODE        0x00
-#define RAM_PAGEMODE        0x80
-*/
-#define RAM_SEQUENTIALMODE  0x40
-
+#elif (MEMORYBOARD_VERSION == 4)
+#define CS0     2
+#define CS1     3
+#define CS2     4
+#else
+#error unknown MEMORYBOARD_VERSION
+#endif
 
 #define RAM_SIZE              (RAM_CHIP_SIZE * RAM_NUM_CHIPS)
 #define RAM_CHIP_SIZE_MASK    (RAM_CHIP_SIZE - 1)
 #define RAM_SIZE_MASK         (RAM_SIZE - 1)
 
 
-//static
-DmaSpi0::Transfer ram_trx(nullptr, 0, nullptr);
+#define MEMORYBOARD_SPICONFIG SPISettings(20000000, MSBFIRST, SPI_MODE0 ) //Speed. 23LC1024 = 20MHz max, see Datasheet.
+#define MEMORYBOARD_SCK   	14
+#define MEMORYBOARD_MOSI   	 7
+#define MEMORYBOARD_MISO  	12
 
-static inline 
+//RAM-Instructions
+#define RAM_INSTR_READ      0x03
+#define RAM_INSTR_WRITE     0x02
+#define RAM_INSTR_EDIO      0x3B
+#define RAM_INSTR_EQIO      0x38
+#define RAM_INSTR_RSTIO     0xFF
+#define RAM_INSTR_RDMR      0x05
+#define RAM_INSTR_WRMR      0x01
+
+#define RAM_BYTEMODE        0x00
+#define RAM_PAGEMODE        0x80
+#define RAM_SEQUENTIALMODE  0x40
+
+static DMAChannel dmatx(true);
+static DMAChannel dmarx(true);
+static DMAMEM uint8_t dummy;
+static volatile bool done;
+
+static inline
 void csrelease(void)
-{  
-  digitalWriteFast(CS0, LOW); digitalWriteFast(CS1, LOW); digitalWriteFast(CS2, LOW); 
+{
+  digitalWriteFast(CS0, LOW); digitalWriteFast(CS1, LOW); digitalWriteFast(CS2, LOW);
 }
 
 static inline
 void csassert(const int cs)
 {
-  switch(cs) {  
+  switch(cs) {
     default:{ digitalWriteFast(CS0, LOW);  digitalWriteFast(CS1, LOW);  digitalWriteFast(CS2, LOW);  break; }
     case 1: { digitalWriteFast(CS0, HIGH); digitalWriteFast(CS1, LOW);  digitalWriteFast(CS2, LOW);  break; }
     case 2: { digitalWriteFast(CS0, LOW);  digitalWriteFast(CS1, HIGH); digitalWriteFast(CS2, LOW);  break; }
     case 3: { digitalWriteFast(CS0, HIGH); digitalWriteFast(CS1, HIGH); digitalWriteFast(CS2, LOW);  break; }
-    case 4: { digitalWriteFast(CS0, LOW);  digitalWriteFast(CS1, LOW);  digitalWriteFast(CS2, HIGH); break; }  
+    case 4: { digitalWriteFast(CS0, LOW);  digitalWriteFast(CS1, LOW);  digitalWriteFast(CS2, HIGH); break; }
     case 5: { digitalWriteFast(CS0, HIGH); digitalWriteFast(CS1, LOW);  digitalWriteFast(CS2, HIGH); break; }
-    case 6: { digitalWriteFast(CS0, LOW);  digitalWriteFast(CS1, HIGH); digitalWriteFast(CS2, HIGH); break; }    
-  //case 7: { digitalWriteFast(CS0, HIGH); digitalWriteFast(CS1, HIGH); digitalWriteFast(CS2, HIGH); break; }    
-  }  
-  
-}
+    case 6: { digitalWriteFast(CS0, LOW);  digitalWriteFast(CS1, HIGH); digitalWriteFast(CS2, HIGH); break; }
+  //case 7: { digitalWriteFast(CS0, HIGH); digitalWriteFast(CS1, HIGH); digitalWriteFast(CS2, HIGH); break; }
+  }
 
-
-//Read Mode Register
-int ram_readRDMR(const int RamNo) 
-{
-  int ret;
-  SPI.beginTransaction(SPICONFIG);
-  csassert(RamNo);
-  SPI.transfer(RAM_INSTR_RDMR);
-  ret = SPI.transfer(0);
-  csrelease();
-  SPI.endTransaction();
-  return ret;
-}
-
-//Write Mode Register
-void ram_writeRDMR(uint8_t mode, const int RamNo)
-{
-  SPI.beginTransaction(SPICONFIG);
-  csassert(RamNo);
-  SPI.transfer16(RAM_INSTR_WRMR << 8 | mode);
-  csrelease();
-  SPI.endTransaction();
-}
-
-
-static inline 
-void ram_startRead(const uint8_t cs, const uint32_t addr)
-{
-  uint32_t tmp;
-  SPI.beginTransaction(SPICONFIG);
-  csassert(cs); 
-  tmp = (RAM_INSTR_READ << 24) | (addr & RAM_CHIP_SIZE_MASK);
-  SPI.transfer16( tmp>>16 );
-  SPI.transfer16( tmp );   
-}
-
-static inline 
-void ram_startWrite(const uint8_t cs, const uint32_t addr)
-{
-  uint32_t tmp;
-  SPI.beginTransaction(SPICONFIG);
-  csassert(cs); 
-  tmp = (RAM_INSTR_WRITE << 24) | (addr & RAM_CHIP_SIZE_MASK);
-  SPI.transfer16( tmp>>16 );
-  SPI.transfer16( tmp );   
 }
 
 static inline
 void ram_stop(void)
 {
-  csrelease();
-  SPI.endTransaction(); 
+	csrelease();
+	SPI.endTransaction();
+}
+
+//Read Mode Register
+int ram_readRDMR(const int RamNo)
+{
+	int ret;
+	SPI.beginTransaction(MEMORYBOARD_SPICONFIG);
+	csassert(RamNo);
+	SPI.transfer(RAM_INSTR_RDMR);
+	ret = SPI.transfer(0);
+	ram_stop();
+	return ret;
+}
+
+//Write Mode Register
+void ram_writeRDMR(uint8_t mode, const int RamNo)
+{
+	SPI.beginTransaction(MEMORYBOARD_SPICONFIG);
+	csassert(RamNo);
+	SPI.transfer16(RAM_INSTR_WRMR << 8 | mode);
+	ram_stop();	
 }
 
 
-/*
-void ram_read8(const uint8_t cs, uint8_t *buf, const uint32_t addr, size_t len)
-{ 
-  ram_startRead(cs, addr);
- 
-  while(len--) {
-    *buf++ = SPI.transfer(0);
-    }
-  
-  ram_stop();
+static inline
+void ram_startRead(const uint8_t cs, const uint32_t addr)
+{
+	uint32_t tmp;
+	SPI.beginTransaction(MEMORYBOARD_SPICONFIG);
+	csassert(cs);
+	tmp = (RAM_INSTR_READ << 24) | (addr & RAM_CHIP_SIZE_MASK);
+	SPI.transfer16( tmp>>16 );
+	SPI.transfer16( tmp );
 }
 
-void ram_write8(uint8_t cs, uint8_t *buf, const uint32_t addr, size_t len)
-{  
-  ram_startWrite(cs, addr);
- 
-  while(len--) {
-    SPI.transfer(*buf++);
-    }
-
-  ram_stop();
+static inline
+void ram_startWrite(const uint8_t cs, const uint32_t addr)
+{
+	uint32_t tmp;
+	SPI.beginTransaction(MEMORYBOARD_SPICONFIG);
+	csassert(cs);
+	tmp = (RAM_INSTR_WRITE << 24) | (addr & RAM_CHIP_SIZE_MASK);
+	SPI.transfer16( tmp>>16 );
+	SPI.transfer16( tmp );
 }
-*/ 
 
 void ram_read16(const uint8_t cs, uint8_t *buf, const uint32_t addr, size_t len)
-{ 
-  if (len == 0) return;
+{
+	if (len == 0) return;	
+	ram_startRead(cs, addr);
   
-  ram_startRead(cs, addr);
-  
-  //align with 8 BIT Transfer
-  if ((uintptr_t)buf & 0x01) {
-   *buf++ = SPI.transfer(0);
-   len--; 
-  }
+	//align with 8 BIT Transfer
+	if ((uintptr_t)buf & 0x01) {
+		*buf++ = SPI.transfer(0);
+		len--;
+	}
 
-  //16BIT Transfer
-  while(len > 1) {
-    *(uint16_t*)buf = __REV16(SPI.transfer16(0));
-    buf += 2;
-    len -= 2;
-  }
-  
+	//16BIT Transfer
+	while(len > 1) {
+		*(uint16_t*)buf = __REV16(SPI.transfer16(0));
+		buf += 2;
+		len -= 2;
+	}
+
   //8BIT Transfer
-  while(len > 0) {
-    *buf++ = SPI.transfer(0);
-    len--;
-  }
-  
- ram_stop();
+	while(len > 0) {
+		*buf++ = SPI.transfer(0);
+		len--;
+	}
+
+	ram_stop();
 }
 
 
 void ram_write16(uint8_t cs, uint8_t *buf, const uint32_t addr, size_t len)
-{ 
-  if (len == 0) return;
-  
-  ram_startWrite(cs, addr); 
+{
+	if (len == 0) return;
+	ram_startWrite(cs, addr);
 
    //align with 8 BIT Transfer
-  if ((uintptr_t)buf & 0x01) {
-   SPI.transfer(*buf++);
-   len--; 
-  }
-  
-  //16BIT Transfer
-  while(len > 1) {
-    SPI.transfer16(__REV16(*(uint16_t*)buf));
-    buf += 2;
-    len -= 2;
-  }  
-  
-  //8BIT Transfer
-  while(len > 0) {
-    SPI.transfer(*buf++);
-    len--;
-  }
-  
-  ram_stop();  
+	if ((uintptr_t)buf & 0x01) {
+		SPI.transfer(*buf++);
+		len--;
+	}
+
+	//16BIT Transfer
+	while(len > 1) {
+		SPI.transfer16(__REV16(*(uint16_t*)buf));
+		buf += 2;
+		len -= 2;
+	}
+
+	//8BIT Transfer
+	while(len > 0) {
+		SPI.transfer(*buf++);
+		len--;
+	}
+
+	ram_stop();
 }
 
-inline
-void ram_waitDMA(void)
+
+//waitFifoEmpty taken from ILI9341_t3.h, (c) Paul Stoffregen
+inline void waitFifoEmpty(void) {
+	uint32_t sr;
+	uint32_t tmp __attribute__((unused));
+	do {
+		sr = KINETISK_SPI0.SR;
+		if (sr & 0xF0) tmp = KINETISK_SPI0.POPR;  // drain RX FIFO
+	} while ((sr & 0xF0F0) > 0);             // wait both RX & TX empty
+}
+
+static void rxIsr(void)
 {
-  while(ram_trx.busy()) {;} 
+	dmarx.clearInterrupt();
+	done = true;
 }
 
+inline void ram_waitDMA(void)
+{ //This does not work with 144 MHZ. WHY? TODO.
+	while(!done) {asm volatile("wfi");};
+	ram_stop();
+}
 
 inline
 void ram_readDMA_noWait(uint8_t cs, uint8_t *buf, const uint32_t addr, size_t len)
 {
-    ram_startRead(cs, addr); 
-    
-    ram_trx = DmaSpi::Transfer(nullptr, len, buf, 0x00);
-    DMASPI0.registerTransfer(ram_trx);
-        
+    ram_startRead(cs, addr);
+    dmatx.source(*(uint8_t*)&dummy);
+	dmatx.transferCount(len);
+	dmarx.destinationBuffer(buf, len);
+	done = false;
+	waitFifoEmpty();
+    SPI0_SR = 0xFF0F0000;
+	SPI0_MCR |= SPI_MCR_CLR_RXF | SPI_MCR_CLR_TXF;
+	SPI0_RSER = SPI_RSER_TFFF_RE | SPI_RSER_TFFF_DIRS | SPI_RSER_RFDF_RE | SPI_RSER_RFDF_DIRS;
+	dmatx.clearComplete();
+	dmarx.enable();
+	dmatx.enable();
 }
 
 void ram_readDMA(uint8_t cs, uint8_t *buf, const uint32_t addr, size_t len)
 {
-
     ram_readDMA_noWait(cs, buf, addr, len);
-    ram_waitDMA();
-    
-    ram_stop();  
+	ram_waitDMA();
 }
 
 inline
 void ram_writeDMA_noWait(uint8_t cs, uint8_t *buf, const uint32_t addr, size_t len)
 {
-    ram_startWrite(cs, addr); 
-    
-    ram_trx = DmaSpi::Transfer(buf, len, nullptr);
-    DMASPI0.registerTransfer(ram_trx);
-      
+    ram_startWrite(cs, addr);
+	dmatx.sourceBuffer(buf, len);
+	dmarx.destination(*(uint8_t*)&dummy);
+	dmarx.transferCount(len);
+	done = false;
+	waitFifoEmpty();
+	SPI0_RSER = SPI_RSER_TFFF_RE | SPI_RSER_TFFF_DIRS | SPI_RSER_RFDF_RE | SPI_RSER_RFDF_DIRS;
+	dmatx.clearComplete();
+	dmarx.enable();
+	dmatx.enable();
 }
 
 void ram_writeDMA(uint8_t cs, uint8_t *buf, const uint32_t addr, size_t len)
 {
-
     ram_writeDMA_noWait(cs, buf, addr, len);
     ram_waitDMA();
-    
-    ram_stop();  
 }
 
 
 void ram_read(uint8_t cs, uint8_t *buf, const uint32_t addr, size_t len)
 {
-    if (len==0) return;
-    ram_waitDMA();
-    if ( len <= 16 ) 
+    if ( len==0 ) return;
+    if ( len <= 16 ) //short transfers are faster without dma
       ram_read16(cs, buf, addr, len);
-    else 
-      ram_readDMA(cs, buf, addr, len);     
+    else
+      ram_readDMA(cs, buf, addr, len);
 }
 
 void ram_write(uint8_t cs, uint8_t *buf, const uint32_t addr, size_t len)
 {
-    if (len==0) return;
-    ram_waitDMA();
-    if ( len <= 16 ) 
+    if ( len==0 ) return;
+	if ( len <= 16 ) //short transfers are faster without dma
       ram_write16(cs, buf, addr, len);
     else 
-      ram_writeDMA(cs, buf, addr, len);     
+      ram_writeDMA(cs, buf, addr, len);
 }
 
 void memoryboard_init(void)
 {
- pinMode(CS0, OUTPUT);
- pinMode(CS1, OUTPUT);
- pinMode(CS2, OUTPUT);
+	pinMode(CS0, OUTPUT);
+	pinMode(CS1, OUTPUT);
+	pinMode(CS2, OUTPUT);
 
- csrelease();  
-  
- SPI.setMOSI(MOSI);
- SPI.setMISO(MISO);
- SPI.setSCK(SCK);
- SPI.begin(); 
+	csrelease();
 
- for (int num=1; num <= RAM_NUM_CHIPS; num++) 
-   ram_writeRDMR( RAM_SEQUENTIALMODE, num);
+	SPI.setMOSI(MEMORYBOARD_MOSI);
+	SPI.setMISO(MEMORYBOARD_MISO);
+	SPI.setSCK(MEMORYBOARD_SCK);
+	SPI.begin();
 
- DMASPI0.begin();
- DMASPI0.start();
-   
+	for (int num=1; num <= RAM_NUM_CHIPS; num++)
+		ram_writeRDMR( RAM_SEQUENTIALMODE, num);
+
+	//DMA
+	SIM_SCGC6 |= SIM_SCGC6_DMAMUX;
+	SIM_SCGC7 |= SIM_SCGC7_DMA;
+
+	//dmatx.begin(true);
+	dmatx.disable();
+	dmatx.destination(*(volatile uint8_t*)&SPI0_PUSHR);
+	dmatx.triggerAtHardwareEvent( DMAMUX_ENABLE | DMAMUX_SOURCE_SPI0_TX );
+	dmatx.disableOnCompletion();
+
+	//dmarx.begin(true);
+	dmarx.disable();
+	dmarx.attachInterrupt(rxIsr);
+	dmarx.interruptAtCompletion();
+	dmarx.source(*(volatile uint8_t*)&SPI0_POPR);
+	dmarx.triggerAtHardwareEvent( DMAMUX_SOURCE_SPI0_RX );
+	dmarx.disableOnCompletion();
+
+	done = false;
 }
 
-#undef CS0 
-#undef CS1
-#undef CS2
-#undef SCK
-#undef MISO
-#undef MOSI
 #endif
-
