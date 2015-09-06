ed1e85d882bdbf94af10848e0025f7aee4b3b853
Frank Bösing <f.boesing@gmx.de>
2015-09-06 13:29:15 +0200

    macro magic

--- MEMORY_BOARD_TEST_INO/memoryboard.h
+++ MEMORY_BOARD_TEST_INO/memoryboard.h
@@ -31,9 +31,7 @@
 // Edit here:
 
 #define MEMORYBOARD_VERSION 3
-
 #define RAM_NUM_CHIPS         (4) //# of RAMs
-#define RAM_CHIP_SIZE         (128*1024) //Size per RAM
 
 //End "Edit here"
 /**************************/
@@ -44,17 +42,18 @@
 #include <arm_math.h>
 
 #if (MEMORYBOARD_VERSION < 4)
-#define CS0     8
-#define CS1    17
-#define CS2    16
+#define MEMORYBOARD_CS0     8
+#define MEMORYBOARD_CS1    17
+#define MEMORYBOARD_CS2    16
 #elif (MEMORYBOARD_VERSION == 4)
-#define CS0     2
-#define CS1     3
-#define CS2     4
+#define MEMORYBOARD_CS0     2
+#define MEMORYBOARD_CS1     3
+#define MEMORYBOARD_CS2     4
 #else
 #error unknown MEMORYBOARD_VERSION
 #endif
 
+#define RAM_CHIP_SIZE         (128*1024) //Size per RAM
 #define RAM_SIZE              (RAM_CHIP_SIZE * RAM_NUM_CHIPS)
 #define RAM_CHIP_SIZE_MASK    (RAM_CHIP_SIZE - 1)
 #define RAM_SIZE_MASK         (RAM_SIZE - 1)
@@ -78,37 +77,67 @@
 #define RAM_PAGEMODE        0x80
 #define RAM_SEQUENTIALMODE  0x40
 
+#if (RAM_NUM_CHIPS < 0) || (RAM_NUM_CHIPS > 6)
+#error please check RAM_NUM_CHIPS
+#endif
+
+#define MEMORYBOARD_PINMODE0	  pinMode(MEMORYBOARD_CS0, OUTPUT)
+#define MEMORYBOARD_PINMODE1
+#define MEMORYBOARD_PINMODE2
+#define MEMORYBOARD_SETCS0(state) digitalWriteFast(MEMORYBOARD_CS0, state)
+#define MEMORYBOARD_SETCS1(state)
+#define MEMORYBOARD_SETCS2(state)
+#if RAM_NUM_CHIPS > 1
+#undef MEMORYBOARD_PINMODE1
+#undef MEMORYBOARD_SETCS1
+#define MEMORYBOARD_PINMODE1	  pinMode(MEMORYBOARD_CS1, OUTPUT)
+#define MEMORYBOARD_SETCS1(state) digitalWriteFast(MEMORYBOARD_CS1, state)
+#endif
+#if RAM_NUM_CHIPS > 3
+#undef MEMORYBOARD_PINMODE2
+#undef MEMORYBOARD_SETCS2
+#define MEMORYBOARD_PINMODE2	  {pinMode(MEMORYBOARD_CS2, OUTPUT);}
+#define MEMORYBOARD_SETCS2(state) digitalWriteFast(MEMORYBOARD_CS2, state)
+#endif
+
+#define CSRELEASE				MEMORYBOARD_SETCS0(LOW);MEMORYBOARD_SETCS1(LOW);MEMORYBOARD_SETCS2(LOW)
+
 static DMAChannel dmatx(true);
 static DMAChannel dmarx(true);
 static DMAMEM uint8_t dummy;
 static volatile bool done;
 
 static inline
-void csrelease(void)
-{
-  digitalWriteFast(CS0, LOW); digitalWriteFast(CS1, LOW); digitalWriteFast(CS2, LOW);
-}
-
-static inline
 void csassert(const int cs)
 {
   switch(cs) {
-    default:{ digitalWriteFast(CS0, LOW);  digitalWriteFast(CS1, LOW);  digitalWriteFast(CS2, LOW);  break; }
-    case 1: { digitalWriteFast(CS0, HIGH); digitalWriteFast(CS1, LOW);  digitalWriteFast(CS2, LOW);  break; }
-    case 2: { digitalWriteFast(CS0, LOW);  digitalWriteFast(CS1, HIGH); digitalWriteFast(CS2, LOW);  break; }
-    case 3: { digitalWriteFast(CS0, HIGH); digitalWriteFast(CS1, HIGH); digitalWriteFast(CS2, LOW);  break; }
-    case 4: { digitalWriteFast(CS0, LOW);  digitalWriteFast(CS1, LOW);  digitalWriteFast(CS2, HIGH); break; }
-    case 5: { digitalWriteFast(CS0, HIGH); digitalWriteFast(CS1, LOW);  digitalWriteFast(CS2, HIGH); break; }
-    case 6: { digitalWriteFast(CS0, LOW);  digitalWriteFast(CS1, HIGH); digitalWriteFast(CS2, HIGH); break; }
-  //case 7: { digitalWriteFast(CS0, HIGH); digitalWriteFast(CS1, HIGH); digitalWriteFast(CS2, HIGH); break; }
+    default:{ CSRELEASE;  break; }
+    case 1: { MEMORYBOARD_SETCS0(HIGH); MEMORYBOARD_SETCS1(LOW);  MEMORYBOARD_SETCS2(LOW);  break; }
+#if RAM_NUM_CHIPS > 1
+    case 2: { MEMORYBOARD_SETCS0(LOW);  MEMORYBOARD_SETCS1(HIGH); MEMORYBOARD_SETCS2(LOW);  break; }
+#if RAM_NUM_CHIPS > 2
+    case 3: { MEMORYBOARD_SETCS0(HIGH); MEMORYBOARD_SETCS1(HIGH); MEMORYBOARD_SETCS2(LOW);  break; }
+#if RAM_NUM_CHIPS > 3
+    case 4: { MEMORYBOARD_SETCS0(LOW);  MEMORYBOARD_SETCS1(LOW);  MEMORYBOARD_SETCS2(HIGH); break; }
+#if RAM_NUM_CHIPS > 4
+    case 5: { MEMORYBOARD_SETCS0(HIGH); MEMORYBOARD_SETCS1(LOW);  MEMORYBOARD_SETCS2(HIGH); break; }
+#if RAM_NUM_CHIPS > 5
+    case 6: { MEMORYBOARD_SETCS0(LOW);  MEMORYBOARD_SETCS1(HIGH); MEMORYBOARD_SETCS2(HIGH); break; }
+#if RAM_NUM_CHIPS > 6	
+    case 7: { MEMORYBOARD_SETCS0(HIGH); MEMORYBOARD_SETCS1(HIGH); MEMORYBOARD_SETCS2(HIGH); break; }
+#endif  
+#endif
+#endif
+#endif
+#endif
+#endif
   }
-
 }
 
 static inline
 void ram_stop(void)
 {
-	csrelease();
+	CSRELEASE;
 	SPI.endTransaction();
 }
 
@@ -130,7 +159,7 @@ void ram_writeRDMR(uint8_t mode, const int RamNo)
 	SPI.beginTransaction(MEMORYBOARD_SPICONFIG);
 	csassert(RamNo);
 	SPI.transfer16(RAM_INSTR_WRMR << 8 | mode);
-	ram_stop();	
+	ram_stop();
 }
 
 
@@ -158,9 +187,9 @@ void ram_startWrite(const uint8_t cs, const uint32_t addr)
 
 void ram_read16(const uint8_t cs, uint8_t *buf, const uint32_t addr, size_t len)
 {
-	if (len == 0) return;	
+	if (len == 0) return;
 	ram_startRead(cs, addr);
-  
+
 	//align with 8 BIT Transfer
 	if ((uintptr_t)buf & 0x01) {
 		*buf++ = SPI.transfer(0);
@@ -228,7 +257,8 @@ static void rxIsr(void)
 	done = true;
 }
 
-inline void ram_waitDMA(void)
+inline 
+void ram_waitDMA(void)
 { //This does not work with 144 MHZ. WHY? TODO.
 	while(!done) {asm volatile("wfi");};
 	ram_stop();
@@ -257,7 +287,6 @@ void ram_readDMA(uint8_t cs, uint8_t *buf, const uint32_t addr, size_t len)
 	ram_waitDMA();
 }
 
-inline
 void ram_writeDMA_noWait(uint8_t cs, uint8_t *buf, const uint32_t addr, size_t len)
 {
     ram_startWrite(cs, addr);
@@ -281,7 +310,7 @@ void ram_writeDMA(uint8_t cs, uint8_t *buf, const uint32_t addr, size_t len)
 
 void ram_read(uint8_t cs, uint8_t *buf, const uint32_t addr, size_t len)
 {
-    if ( len==0 ) return;
+    if ( len == 0 ) return;
     if ( len <= 16 ) //short transfers are faster without dma
       ram_read16(cs, buf, addr, len);
     else
@@ -290,20 +319,20 @@ void ram_read(uint8_t cs, uint8_t *buf, const uint32_t addr, size_t len)
 
 void ram_write(uint8_t cs, uint8_t *buf, const uint32_t addr, size_t len)
 {
-    if ( len==0 ) return;
+    if ( len == 0 ) return;
 	if ( len <= 16 ) //short transfers are faster without dma
       ram_write16(cs, buf, addr, len);
-    else 
+    else
       ram_writeDMA(cs, buf, addr, len);
 }
 
 void memoryboard_init(void)
 {
-	pinMode(CS0, OUTPUT);
-	pinMode(CS1, OUTPUT);
-	pinMode(CS2, OUTPUT);
 
-	csrelease();
+	MEMORYBOARD_PINMODE0;
+	MEMORYBOARD_PINMODE1;
+	MEMORYBOARD_PINMODE2;
+	CSRELEASE;
 
 	SPI.setMOSI(MEMORYBOARD_MOSI);
 	SPI.setMISO(MEMORYBOARD_MISO);
